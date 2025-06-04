f0d81080b02afd0c898a658f94729a8ff591c649
Omixo <alluomi55@gmail.com>
2025-06-04 08:36:14 +0530

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import ProductList from './components/ProductList'
+import Cart from './components/Cart'
 import './App.css'
 
 function App() {
